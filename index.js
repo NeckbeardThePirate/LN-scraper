88aa7bba8c5202035a8fd8889bda9f525e54ada7
Judah Helland Sr. <judah@mcmahan.tech>
2025-04-30 06:36:03 -0700

    it works

--- index.js
+++ index.js
@@ -1,36 +1,44 @@
-import FirecrawlApp, { CrawlParams, CrawlStatusResponse } from '@mendable/firecrawl-js';
+const firecrawl = require('@mendable/firecrawl-js'); // Import the module
+const FirecrawlApp = firecrawl.default; // Access the default export
 
-const app = new FirecrawlApp({apiKey: "fc-YOUR_API_KEY"});
+const dotenv = require('dotenv/config');
+const fs = require('fs');
 
-// Scrape a website
-const scrapeResponse = await app.scrapeUrl('https://firecrawl.dev', {
-  formats: ['markdown', 'html'],
-});
+// const app = new FirecrawlApp({ apiKey: "fc-YOUR_API_KEY" });
 
-if (!scrapeResponse.success) {
-  throw new Error(`Failed to scrape: ${scrapeResponse.error}`)
+// // Scrape a website:
+// (async () => {
+//   try {
+//     const scrapeResult = await app.scrapeUrl('firecrawl.dev', { formats: ['markdown', 'html'] });
+
+//     if (!scrapeResult.success) {
+//       throw new Error(`Failed to scrape: ${scrapeResult.error}`);
+//     }
+
+//     console.log(scrapeResult);
+//   } catch (error) {
+//     console.error(error);
+//   }
+// })();
+
+
+
+
+
+const app = new FirecrawlApp({ apiKey: process.env.FC_Key });
+
+async function doAScrape() {
+	const scrapeResponse = await app.scrapeUrl('https://notthebee.com/article/watch-president-trump-was-in-rare-form-as-he-schooled-antagonistic-abc-news-interviewer', {
+		formats: ['markdown'],
+	});
+
+	if (!scrapeResponse.success) {
+		throw new Error(`Failed to scrape: ${scrapeResponse.error}`);
+	}
+
+    fs.writeFileSync('./response.json', JSON.stringify(scrapeResponse, null, 4))
+
+	console.log(scrapeResponse);
 }
 
-console.log(scrapeResponse)
-
-// // Crawl a website
-// const crawlResponse = await app.crawlUrl('https://firecrawl.dev', {
-//   limit: 100,
-//   scrapeOptions: {
-//     formats: ['markdown', 'html'],
-//   }
-// });
-
-// if (!crawlResponse.success) {
-//   throw new Error(`Failed to crawl: ${crawlResponse.error}`)
-// }
-
-// // console.log(crawlResponse)
-
-// const scrapeResult = await app.scrapeUrl('firecrawl.dev', { formats: ['markdown', 'html'] });
-
-// if (!scrapeResult.success) {
-//   throw new Error(`Failed to scrape: ${scrapeResult.error}`)
-// }
-
-// console.log(scrapeResult)
+doAScrape()
