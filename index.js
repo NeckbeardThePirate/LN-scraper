a41e43c75461828e561f382c1c9e2b14448a4b30
Judah Helland Sr. <judah@mcmahan.tech>
2025-04-30 09:45:17 -0700

    transfer

--- index.js
+++ index.js
@@ -36,9 +36,57 @@ async function doAScrape() {
 		throw new Error(`Failed to scrape: ${scrapeResponse.error}`);
 	}
 
-    fs.writeFileSync('./response.json', JSON.stringify(scrapeResponse, null, 4))
-
-	console.log(scrapeResponse);
+    fs.writeFileSync('./response.json', JSON.stringify(scrapeResponse.markdown, null, 4))
+    const summation = await makeASummary(scrapeResponse.markdown)
+    fs.writeFileSync('./summation.json', )
+	
+    console.log(summation);
 }
 
-doAScrape()
+// doAScrape()
+
+
+async function makeASummary(text) {
+    const url = `https://gateway.ai.cloudflare.com/v1/${process.env.ENDPOINT_ID}/test-gateway/workers-ai/@cf/meta/llama-3.1-8b-instruct`
+
+    const response = await fetch(url, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${process.env.API_TOKEN}`
+        },
+        body: JSON.stringify({
+                    provider: 'workers-ai',
+                    //   endpoint: "@cf/qwen/qwen1.5-7b-chat-awq",
+                    //   endpoint: "@cf/meta/llama-2-7b-chat-fp16",
+                    endpoint: '@cf/meta/llama-3.1-8b-instruct-fp8-fast',
+                    //   endpoint: "@cf/meta/llama-2-7b-chat-fp16",
+        
+                    headers: {
+                        Authorization: `Bearer ${myToken}`,
+                        'Content-Type': 'application/json',
+                    },
+                    query: {
+                        messages: [
+                            {
+                                role: 'system',
+                                content: `Your job is to take each task you receive and break it into ${subItems} smaller comma separated generic subtasks, the smaller tasks should be comma delimited and there should be ${subItems}, do not do or say anything other than the returned set of comma delimited subtasks. For instance: 'Change Babys Diaper': Gather Supplies, Take off outfit, Undo diaper, Put on new diaper, Put outfit back on. They should be in the most proper and helpful order possible, when in doubt greater specificity is better`,
+                            },
+                            {
+                                role: 'user',
+                                content: `${task}`,
+                            },
+                        ],
+                    },
+            prompt: `Summarize this news story like an old school tweet: ${text}`
+        })
+    })
+    console.log(await response.json())
+    // return await response.json()
+
+}
+
+
+const sample = fs.readFileSync('response.md', 'utf8');
+const sumSample = makeASummary(sample)
+
